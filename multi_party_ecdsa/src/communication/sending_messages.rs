81c13174b048d185dc3b54e046c43764103d1441
hossein <hossein.rezaei.ghaleh@gmail.com>
2025-11-21 23:44:04 +0000

    remove p2p

--- multi_party_ecdsa/src/communication/sending_messages.rs
+++ multi_party_ecdsa/src/communication/sending_messages.rs
@@ -4,7 +4,6 @@ use std::collections::HashMap;
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum SendingMessages {
     NormalMessage(usize, Vec<u8>),
-    P2pMessage(HashMap<usize, Vec<u8>>),
     SubsetMessage(Vec<u8>),
     BroadcastMessage(Vec<u8>),
     EmptyMsg,
