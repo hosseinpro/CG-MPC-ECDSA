949ad830a75724ea7bb3ceaa9f1c19c26b84253f
hossein <hossein.rezaei.ghaleh@gmail.com>
2025-11-22 00:15:00 +0000

    remove

--- multi_party_ecdsa/src/communication/receiving_messages.rs
+++ multi_party_ecdsa/src/communication/receiving_messages.rs
@@ -9,13 +9,4 @@ pub enum ReceivingMessages {
     XAXTwoSignMessagePartyOne(XAXPartyOneMsg),
     XAXTwoKeyGenMessagePartyTwo(XAXPartyTwoMsg),
     XAXTwoSignMessagePartyTwo(XAXPartyTwoMsg),
-    KeyGenBegin,
-    SignBegin,
-    KeyRefreshBegin,
-    SetMessage(String),
-    SignOnlineBegin,
-    TwoPartySignRefresh(String, String), // SignRefresh(message, keygen_result_json)
-    MultiPartySignRefresh(String, String, String, Vec<usize>), // SignRefresh(message, keygen_result_json, subset)
-    // TBD: Extend it to errors
-    NeedRefresh,
 }
--- multi_party_ecdsa/src/communication/sending_messages.rs
+++ multi_party_ecdsa/src/communication/sending_messages.rs
@@ -2,11 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum SendingMessages {
-    NormalMessage(usize, Vec<u8>),
-    SubsetMessage(Vec<u8>),
     BroadcastMessage(Vec<u8>),
     EmptyMsg,
     KeyGenSuccessWithResult(Vec<String>),
-    KeyRefreshSuccessWithResult(Vec<String>),
     SignSuccessWithResult(String),
 }
