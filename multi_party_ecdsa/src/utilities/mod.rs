ae93f2551895376b38b73c91232129b8cb761254
hossein <hossein.rezaei.ghaleh@gmail.com>
2025-11-22 00:22:09 +0000

    remove

--- multi_party_ecdsa/src/utilities/mod.rs
+++ multi_party_ecdsa/src/utilities/mod.rs
@@ -1,16 +1,10 @@
 pub const SECURITY_BITS: usize = 256;
 pub const SECURITY_PARAMETER: usize = 128;
 
-//pub mod cl_enc_proof;
-//pub mod class;
-pub mod cl_dl_proof;
 pub mod class_group;
 pub mod clkeypair;
 pub mod dl_com_zk;
 pub mod eckeypair;
-pub mod elgamal;
 pub mod error;
-pub mod promise_sigma;
-pub mod promise_sigma_multi;
 pub mod signature;
 pub mod cl_proof;
