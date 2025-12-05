44345caaf1b1dc4582c0c1c03a77669dab7d2af8
hossein <hossein.rezaei.ghaleh@gmail.com>
2025-12-05 22:16:32 +0000

    remove thiserror

--- multi_party_ecdsa/src/utilities/error.rs
+++ multi_party_ecdsa/src/utilities/error.rs
@@ -1,70 +1,80 @@
-use thiserror::Error;
+use std::fmt;
+use std::error::Error;
 
 /// Represents errors.
-#[derive(Error, Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum MulEcdsaError {
-    #[error("Open dlcommitment failed")]
     OpenDLCommFailed,
-    #[error("Open zk-pok commitment failed")]
     OpenCommZKFailed,
-    #[error("Verify DLog failed")]
     VrfyDlogFailed,
-    #[error("The size of zr excceeds sample size")]
     ZrExcceedSize,
-    #[error("Verify promise sigma protocol failed")]
     VrfyPromiseFailed,
-    #[error("The return of x_coor() in None")]
     XcoorNone,
-    #[error("Verify multi-party ECDSA signature failed")]
     VrfyMultiECDSAFailed,
-    #[error("Verify class group pk failed")]
     VrfyClassGroupFailed,
-    #[error("Get index failed")]
     GetIndexFailed,
-    #[error("Serialize failed ")]
     SerializeFailed,
-    #[error("Verify VSS failed")]
     VrfyVSSFailed,
-    #[error("To string failed")]
     ToStringFailed,
-    #[error("From string failed")]
     FromStringFailed,
-    #[error("Party numbers less than the value of threshold in multiparty ecdsa keygen")]
     PartyLessThanThreshold,
-    #[error("Left not equal to Right")]
     LeftNotEqualRight,
-    #[error("Verify multiparty ecdsa sign phase one message failed")]
     VrfySignPhaseOneMsgFailed,
-    #[error("Handle multiparty ecdsa sign phase two message failed")]
     HandleSignPhaseTwoMsgFailed,
-    #[error("Open general commitment failed")]
     OpenGeCommFailed,
-    #[error("Verify HomoElGamal failed")]
     VrfyHomoElGamalFailed,
-    #[error("Verify sum_a_t failed")]
     VrfySumatFailed,
-    #[error("Compute delta sum msg in multiparty ecdsa sign phase two failed")]
     ComputeDeltaSumFailed,
-    #[error("Verify ElgamalProof failed")]
     VrfyElgamalProofFailed,
-    #[error("Verify CLEncProof failed")]
     VrfyClEncProofFailed,
-    #[error("Verify CLDLProof Failed")]
     VrfyCLDLProofFailed,
-    #[error("Verify CLProof Failed")]
     VrfyCLProofFailed,
-    #[error("Not load keygen result")]
     NotLoadKeyGenResult,
-    #[error("Invalid public key")]
     InvalidPublicKey,
-    #[error("From Hex Failed")]
     FromHexFailed,
-    #[error("Generate Result Json String Failed")]
     GenerateJsonStringFailed,
-    #[error("Missing message to sign")]
     MissingMsg,
-    #[error("Invert a zero element")]
     InvertZero,
-    #[error("General error")]
     GeneralError,
 }
+
+impl fmt::Display for MulEcdsaError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MulEcdsaError::OpenDLCommFailed => write!(f, "Open dlcommitment failed"),
+            MulEcdsaError::OpenCommZKFailed => write!(f, "Open zk-pok commitment failed"),
+            MulEcdsaError::VrfyDlogFailed => write!(f, "Verify DLog failed"),
+            MulEcdsaError::ZrExcceedSize => write!(f, "The size of zr excceeds sample size"),
+            MulEcdsaError::VrfyPromiseFailed => write!(f, "Verify promise sigma protocol failed"),
+            MulEcdsaError::XcoorNone => write!(f, "The return of x_coor() in None"),
+            MulEcdsaError::VrfyMultiECDSAFailed => write!(f, "Verify multi-party ECDSA signature failed"),
+            MulEcdsaError::VrfyClassGroupFailed => write!(f, "Verify class group pk failed"),
+            MulEcdsaError::GetIndexFailed => write!(f, "Get index failed"),
+            MulEcdsaError::SerializeFailed => write!(f, "Serialize failed"),
+            MulEcdsaError::VrfyVSSFailed => write!(f, "Verify VSS failed"),
+            MulEcdsaError::ToStringFailed => write!(f, "To string failed"),
+            MulEcdsaError::FromStringFailed => write!(f, "From string failed"),
+            MulEcdsaError::PartyLessThanThreshold => write!(f, "Party numbers less than the value of threshold in multiparty ecdsa keygen"),
+            MulEcdsaError::LeftNotEqualRight => write!(f, "Left not equal to Right"),
+            MulEcdsaError::VrfySignPhaseOneMsgFailed => write!(f, "Verify multiparty ecdsa sign phase one message failed"),
+            MulEcdsaError::HandleSignPhaseTwoMsgFailed => write!(f, "Handle multiparty ecdsa sign phase two message failed"),
+            MulEcdsaError::OpenGeCommFailed => write!(f, "Open general commitment failed"),
+            MulEcdsaError::VrfyHomoElGamalFailed => write!(f, "Verify HomoElGamal failed"),
+            MulEcdsaError::VrfySumatFailed => write!(f, "Verify sum_a_t failed"),
+            MulEcdsaError::ComputeDeltaSumFailed => write!(f, "Compute delta sum msg in multiparty ecdsa sign phase two failed"),
+            MulEcdsaError::VrfyElgamalProofFailed => write!(f, "Verify ElgamalProof failed"),
+            MulEcdsaError::VrfyClEncProofFailed => write!(f, "Verify CLEncProof failed"),
+            MulEcdsaError::VrfyCLDLProofFailed => write!(f, "Verify CLDLProof Failed"),
+            MulEcdsaError::VrfyCLProofFailed => write!(f, "Verify CLProof Failed"),
+            MulEcdsaError::NotLoadKeyGenResult => write!(f, "Not load keygen result"),
+            MulEcdsaError::InvalidPublicKey => write!(f, "Invalid public key"),
+            MulEcdsaError::FromHexFailed => write!(f, "From Hex Failed"),
+            MulEcdsaError::GenerateJsonStringFailed => write!(f, "Generate Result Json String Failed"),
+            MulEcdsaError::MissingMsg => write!(f, "Missing message to sign"),
+            MulEcdsaError::InvertZero => write!(f, "Invert a zero element"),
+            MulEcdsaError::GeneralError => write!(f, "General error"),
+        }
+    }
+}
+
+impl Error for MulEcdsaError {}
